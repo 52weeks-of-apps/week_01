7169c91f56ad0c66434f0d45f97ab3ce55e78c2a
Trotter Cashion <cashion@gmail.com>
2010-02-06 13:35:49 -0500

    Add the landing controller

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,4 @@
+class LandingController < ApplicationController
+  def root
+  end
+end
---
+++ test/functional/landing_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LandingControllerTest < ActionController::TestCase
+  test "GET /root" do
+    assert_response :success
+  end
+end
