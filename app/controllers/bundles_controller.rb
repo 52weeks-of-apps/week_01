711599921ee1ab7c3bd5a6c9adb63198534eee57
Trotter Cashion <cashion@gmail.com>
2010-02-06 16:20:04 -0500

    Support raw and xml formats

--- app/controllers/bundles_controller.rb
+++ app/controllers/bundles_controller.rb
@@ -3,10 +3,18 @@ class BundlesController < ApplicationController
     @bundle = Bundle.find_or_create_by_url(params[:bundle][:url])
     @bundle.update_body!
     @bundle.save
-    redirect_to bundle_url(@bundle)
+    respond_to do |wants|
+      wants.html { redirect_to bundle_url(@bundle) }
+      wants.xml  { render :xml => @bundle.body, :location => bundle_url(@bundle) }
+    end
   end
 
   def show
     @bundle = Bundle.find(params[:id])
+    respond_to do |wants|
+      wants.html
+      wants.raw { render :text => @bundle.body }
+      wants.xml { render :xml => @bundle.body }
+    end
   end
 end
